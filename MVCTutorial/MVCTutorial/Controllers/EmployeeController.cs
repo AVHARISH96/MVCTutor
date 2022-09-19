805e2f3a94d0761302bd7723911e225eca0c92be
AVHARISH96 <avharish96@gmail.com>
2022-09-19 14:42:51 +0530

    Added Model

--- MVCTutorial/MVCTutorial/Controllers/EmployeeController.cs
+++ MVCTutorial/MVCTutorial/Controllers/EmployeeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MVCTutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace MVCTutorial.Controllers
 {
@@ -33,6 +35,20 @@ namespace MVCTutorial.Controllers
         public ActionResult About()
         {
             return View("~/Views/Custom/Index.cshtml");
+        }
+
+        public ActionResult ShowEmployee(int id)
+        {
+            var data = GetEmployee(id);
+            return View(data);
+        }
+        private Employee GetEmployee(int id)
+        {
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee { Id = 2, Name = "Sony" });
+            employees.Add(new Employee { Id = 5, Name = "Lee" });
+            return employees.Where(e => e.Id == id).FirstOrDefault();
         }
     }
 }
